271a74133b51351f29cf50ccd24dc560ce36f94b
Nathan Silva <nathan.silva@ndevs.com.br>
2024-10-02 05:12:47 -0300

    switch to https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: 'http://api-faisca.online/project342', // Ajuste isso para o ambiente correto
+    baseURL: 'https://api-faisca.online/project342', // Ajuste isso para o ambiente correto
   });
   
